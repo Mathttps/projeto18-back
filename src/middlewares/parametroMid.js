c1bdb4718d62563329740a69ca6d7a43691b63b9
Matheus Henrique <mathttps@gmail.com>
2023-05-29 17:39:47 -0300

    error

--- src/middlewares/parametroMid.js
+++ src/middlewares/parametroMid.js
@@ -1,4 +1,7 @@
 export function validateParams(req, res, next) {
-    
+    const {authoriziation} = req.headers;
+    const token = authoriziation.replace("Bearer ", "")
+
+     if(!token) return res.sendStatus(401);
     next();
     }
